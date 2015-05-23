71d4dc25e113751efb686159cd6adb6504717053
Alexandru Simion <alexandru.simion@endava.com>
2015-05-23 05:05:01 +0300

    added again SC

--- src/main/java/com/endava/controller/ServicesController.java
+++ src/main/java/com/endava/controller/ServicesController.java
@@ -1,13 +1,21 @@
 package com.endava.controller;
 
+import com.endava.dto.MockService;
+import com.endava.dto.Rule;
+import com.endava.persistance.PersistanceDao;
+import com.endava.persistance.PersistanceService;
+//import com.sun.deploy.net.HttpResponse;
+import javassist.NotFoundException;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by astoica on 5/22/2015.
@@ -22,6 +30,9 @@ public class ServicesController {
     private static String returnedIDE2 = "4";
     private static final String VIEW_INDEX = "services";
     private final static org.slf4j.Logger logger = LoggerFactory.getLogger(ServicesController.class);
+    @Autowired
+    PersistanceService persistanceService;
+
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ModelAndView sendJson() {
@@ -31,11 +42,25 @@ public class ServicesController {
         return model;
     }
 
-    @RequestMapping(value="", method=RequestMethod.POST)
-    @ResponseBody
-    public String getID(){
-        return "{id : " +returnedIDE+"}";
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public ResponseEntity<List<MockService>> getAll() {
+        return new ResponseEntity<List<MockService>>(persistanceService.getAllServices(), null, HttpStatus.OK);
     }
+
+    @RequestMapping(value="", method=RequestMethod.POST )
+    public ResponseEntity<String> getID(){
+//        persistanceService.persistService(mockService);
+        return new ResponseEntity<String>(returnedIDE, null, HttpStatus.CREATED);
+    }
+
+//    @RequestMapping(value="", method=RequestMethod.POST)
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public ResponseEntity<List<MockService>> gID(@RequestBody MockService mockService){
+//        System.out.println(mockService);
+//        persistanceService.persistService(mockService);
+//        return new ResponseEntity<List<MockService>>();
+//        return "soemthing";
+//    }
 
     @RequestMapping(value = "/rules", method = RequestMethod.GET)
     public ModelAndView getRulesPage() {
@@ -46,20 +71,19 @@ public class ServicesController {
     }
 
     @RequestMapping(value="/{Id}/rule", method=RequestMethod.GET)
-    public ModelAndView getJsonRule(@PathVariable Integer Id) {
-        ModelAndView model = new ModelAndView("rules");
-        if (Id == 4) {
-            model.addObject("msg", json_string2);
-        } else
-        { model.addObject("msg", json_string);
-        }
-        return model;
+    public List<Rule> getJsonRule(@PathVariable Integer Id) {
+        return persistanceService.getRulesForService(Id);
     }
     @RequestMapping(value="/{Id}/rule", method=RequestMethod.POST)
     @ResponseBody
     public String getRiule(){
         return "{id : " +returnedIDE2 +"}";
     }
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason = "Url not found")
+    void handleNotFound(NotFoundException exc) {}
+
+
 
 }
 
